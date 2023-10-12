8180930c14939ffa1638521234fec8bd788c2eea
fabiopereiraz <fabio.pereira@icarros.com.br>
2023-10-12 18:49:22 -0300

    feat: upload inicial

---
+++ src/main/java/br/com/fabiopereira/desafiouber1/DesafioUber1Application.java
@@ -0,0 +1,13 @@
+package br.com.fabiopereira.desafiouber1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioUber1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioUber1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fabiopereira/desafiouber1/DesafioUber1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabiopereira.desafiouber1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioUber1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
